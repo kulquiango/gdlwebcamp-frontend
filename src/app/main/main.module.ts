f976d62274042d798de17ce4f9019fa05dfac6cc
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-08-12 21:05:31 -0500

    KUG-Actualizacion main

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/components/header/header.component';
+import { MainComponent } from './home/components/main/main.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent, HeaderComponent, MainComponent],
   exports: [HomeComponent],
 })
 export class MainModule {}
