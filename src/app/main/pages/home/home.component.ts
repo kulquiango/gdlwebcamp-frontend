bc0079c371943ced123e09d20c4a0e606a3cc24b
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2022-08-14 17:35:24 -0500

    KUG-Optimización de codigo

--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	socialNetworks = [
+		{ icon: 'fab fa-facebook-square' },
+		{ icon: 'fab fa-twitter' },
+		{ icon: 'fab fa-pinterest' },
+		{ icon: 'fab fa-youtube' },
+		{ icon: 'fab fa-instagram' },
+	]
 }
